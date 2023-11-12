0a19d787d8fe40388abc75e1830e06497b5190c8
Xtenso <stefanbprivate@gmail.com>
2023-11-12 11:50:46 +0200

    Initial commit

---
+++ src/pages/layoutForManager/withMainLayout.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default function withMainLayout(Component){
+    function layout(props){
+        return(
+            <>
+                <div>
+                    
+                </div>
+            </>
+        )
+    }
+}   
