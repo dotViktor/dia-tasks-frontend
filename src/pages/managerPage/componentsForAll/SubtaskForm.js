2a8ae9652ecbb6fe52ebf30768841bf9caa46823
Xtenso <stefanbprivate@gmail.com>
2023-11-22 16:41:47 +0200

    added subtasks functionality

--- src/pages/managerPage/componentsForAll/SubtaskForm.js
+++ src/pages/managerPage/componentsForAll/SubtaskForm.js
@@ -1,10 +1,95 @@
-// SubtaskForm.js
 import React, { useState } from "react";
-import "../createAddEditTasksPage/CreateAddEditTasks.css";
 
-const SubtaskForm = ({ onClose }) => {
+const SubtaskForm = ({ taskParentID, onClose, onAddSubtask }) => {
+  const [subtaskData, setSubtaskData] = useState({
+    TaskParentID: taskParentID,
+    title: "",
+    description: "",
+    requiredImages: 0,
+    requiredNotes: 1,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // Converting value to a number if the name is "requiredNotes" or "requiredImages"
+    const updatedValue =
+      name === "requiredNotes" || name === "requiredImages"
+        ? Number(value)
+        : value;
+
+    setSubtaskData({
+      ...subtaskData,
+      [name]: updatedValue,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Ensure at least one note is required
+    const requiredNotes =
+      subtaskData.requiredNotes < 1 ? 1 : subtaskData.requiredNotes;
+
+    const subtaskWithId = {
+      ...subtaskData,
+      TaskParentID: taskParentID,
+      requiredNotes: requiredNotes,
+    };
+
+    onAddSubtask(subtaskWithId);
+    onClose();
+  };
+
   return (
     <div className="subtask-form">
+      <h2>Add Subtask</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Subtask Name:
+          <br />
+          <input
+            type="text"
+            name="title"
+            value={subtaskData.title}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Subtask Description:
+          <br />
+          <textarea
+            name="description"
+            value={subtaskData.description}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Number of Images:
+          <br />
+          <input
+            type="number"
+            name="requiredImages"
+            value={subtaskData.requiredImages}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Number of Notes (At least 1):
+          <br />
+          <input
+            type="number"
+            name="requiredNotes"
+            value={subtaskData.requiredNotes}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Add Subtask</button>
+      </form>
       <button onClick={onClose}>Close</button>
     </div>
   );
--- src/pages/managerPage/createAddEditTasksPage/CreateAddEditTasks.js
+++ src/pages/managerPage/createAddEditTasksPage/CreateAddEditTasks.js
@@ -41,6 +41,24 @@ const CreateAddEditTasks = () => {
     );
   };
 
+  //Subtask component
+  const Subtask = ({ subtask, onSubtaskDelete }) => {
+    return (
+      <div className="user-manager-container">
+        <h4>{subtask.title}</h4>
+        {subtask.id &&
+          (subtask.isComplete === 0 || subtask.isComplete === undefined) && (
+            <span
+              className="material-symbols-outlined"
+              onClick={() => onSubtaskDelete(subtask)}
+            >
+              close
+            </span>
+          )}
+      </div>
+    );
+  };
+
   const [users, setUsers] = useState([]);
 
   //For all users list
@@ -60,23 +78,74 @@ const CreateAddEditTasks = () => {
     users: [],
   });
 
-  useEffect(() => {
-    if (id) {
-      axios
-        .get(`http://localhost:7777/tasks/${id}`)
-        .then((response) => {
-          const task =
-            Array.isArray(response.data) && response.data.length > 0
-              ? response.data[0]
-              : null;
+  const [subtaskData, setSubtaskData] = useState({
+    newSubtasks: [],
+    deletedSubtasks: [],
+  });
 
-          if (task && task.title) {
-            setTaskData(task);
-          } else {
-            console.error("Invalid API response:", response.data);
-          }
-        })
-        .catch((err) => console.error(err));
+  // useEffect(() => {
+  //   // Fetch task details
+  //   if (id) {
+  //     axios
+  //       .get(`http://localhost:7777/tasks/${id}`)
+  //       .then((response) => {
+  //         const task =
+  //           Array.isArray(response.data) && response.data.length > 0
+  //             ? response.data[0]
+  //             : null;
+
+  //         if (task && task.title) {
+  //           setTaskData(task);
+  //         } else {
+  //           console.error("Invalid API response:", response.data);
+  //         }
+  //       })
+  //       .catch((err) => console.error(err));
+
+  //     // Fetch subtasks
+  //     axios
+  //       .get(`http://localhost:7777/tasks/${id}/subtasks`)
+  //       .then((response) => {
+  //         setTaskData((prevTaskData) => ({
+  //           ...prevTaskData,
+  //           subtasks: response.data,
+  //         }));
+  //       })
+  //       .catch((err) => console.error(err));
+  //   }
+  // }, [id]);
+
+  //Fetching tasks and subtasks
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [taskResponse, subtasksResponse] = await Promise.all([
+          axios.get(`http://localhost:7777/tasks/${id}`),
+          axios.get(`http://localhost:7777/tasks/${id}/subtasks`),
+        ]);
+
+        const task =
+          Array.isArray(taskResponse.data) && taskResponse.data.length > 0
+            ? taskResponse.data[0]
+            : null;
+
+        if (task && task.title) {
+          setTaskData(task);
+        } else {
+          console.error("Invalid API response:", taskResponse.data);
+        }
+
+        setTaskData((prevTaskData) => ({
+          ...prevTaskData,
+          subtasks: subtasksResponse.data,
+        }));
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    if (id) {
+      fetchData();
     }
   }, [id]);
 
@@ -92,7 +161,7 @@ const CreateAddEditTasks = () => {
     });
   };
 
-  //Assigning and unassigning users to a task
+  //Assigning and unassigning users to/from a task
   const handleAssignUser = (user) => {
     if (!taskData.users.some((assignedUser) => assignedUser.id === user.id)) {
       const updatedUsers = [...taskData.users, user];
@@ -139,20 +208,85 @@ const CreateAddEditTasks = () => {
     setShowSubtaskForm(false);
   };
 
+  //Adds subtasks
+  const handleAddSubtask = (subtask) => {
+    const subtaskId = Date.now();
+
+    setSubtaskData((prevSubtaskData) => ({
+      ...prevSubtaskData,
+      newSubtasks: [
+        ...(prevSubtaskData.newSubtasks || []),
+        { ...subtask, id: subtaskId },
+      ],
+    }));
+
+    setTaskData((prevTaskData) => ({
+      ...prevTaskData,
+      subtasks: [
+        ...(prevTaskData.subtasks || []),
+        { ...subtask, id: subtaskId },
+      ],
+    }));
+  };
+
+  //Subtasks delete
+  const handleSubtaskDelete = (subtask) => {
+    // Add the subtask id to deletedSubtasks
+    setSubtaskData((prevSubtaskData) => ({
+      ...prevSubtaskData,
+      deletedSubtasks: [...(prevSubtaskData.deletedSubtasks || []), subtask.id],
+    }));
+
+    // Remove the subtask from the list of subtasks
+    setTaskData((prevTaskData) => ({
+      ...prevTaskData,
+      subtasks: prevTaskData.subtasks.filter((task) => task.id !== subtask.id),
+    }));
+
+    // Remove the subtask from the newSubtasks list
+    setSubtaskData((prevSubtaskData) => ({
+      ...prevSubtaskData,
+      newSubtasks: prevSubtaskData.newSubtasks.filter(
+        (newSubtask) => newSubtask.id !== subtask.id
+      ),
+    }));
+  };
+
+  //Console logs subtasks array
+  useEffect(() => {
+    console.log("Updated taskData.subtasks:", taskData.subtasks);
+  }, [taskData.subtasks]);
+  useEffect(() => {
+    console.log("Updated subtaskData.newSubtasks:", subtaskData.newSubtasks);
+  }, [subtaskData.newSubtasks]);
+
   //Form/task submit
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted Data:", taskData);
 
     if (id) {
+      //For updating tasks
       axios
         .put(`http://localhost:7777/tasks/${id}`, taskData)
         .then((response) => {
           console.log("Task updated:", response.data);
-          navigate(-1);
+
+          //Subtasks array
+          axios
+            .put(
+              `http://localhost:7777/tasks/${id}/update-subtasks`,
+              subtaskData
+            )
+            .then((response) => {
+              console.log("Subtasks created:", response.data);
+              navigate(-1);
+            })
+            .catch((error) => console.error("Create task error:", error));
         })
         .catch((error) => console.error("Update task error:", error));
     } else {
+      //For creating tasks
       axios
         .post("http://localhost:7777/tasks", taskData)
         .then((response) => {
@@ -217,11 +351,29 @@ const CreateAddEditTasks = () => {
             <hr></hr>
             <div className="subtasks">
               <h2>Subtasks</h2>
-              {/* <ul className="subtasks-list">
-                {taskData.subtasks.map((subtask) => (
-                  <li key={subtask.id}>{subtask.name}</li>
-                ))}
-              </ul> */}
+              {taskData.subtasks &&
+                taskData.subtasks
+                  .filter(
+                    (subtask) => subtask.isComplete !== 1 || !subtask.isComplete
+                  )
+                  .map((subtask) => (
+                    <Subtask
+                      key={subtask.id}
+                      subtask={subtask}
+                      onSubtaskDelete={handleSubtaskDelete}
+                    />
+                  ))}
+              <h2>Completed Subtasks</h2>
+              {taskData.subtasks &&
+                taskData.subtasks
+                  .filter((subtask) => subtask.isComplete === 1)
+                  .map((subtask) => (
+                    <Subtask
+                      key={subtask.id}
+                      subtask={subtask}
+                      onSubtaskDelete={handleSubtaskDelete}
+                    />
+                  ))}
               <button type="button" onClick={handleShowSubtaskForm}>
                 Add a subtask
               </button>
@@ -272,7 +424,11 @@ const CreateAddEditTasks = () => {
       </form>
       {showSubtaskForm && (
         <div className="modal-overlay">
-          <SubtaskForm onClose={handleCloseSubtaskForm} />
+          <SubtaskForm
+            taskParentID={Number(id)}
+            onClose={handleCloseSubtaskForm}
+            onAddSubtask={handleAddSubtask}
+          />
         </div>
       )}
     </>
