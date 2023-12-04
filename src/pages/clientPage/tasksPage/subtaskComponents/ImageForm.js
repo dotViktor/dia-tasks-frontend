fa7e411fd07fce244080271fd3eda12994458be2
dotViktor <viktorstoimenov@vivaldi.net>
2023-12-04 11:18:42 +0200

    Imageform hotfixes

--- src/pages/clientPage/tasksPage/subtaskComponents/ImageForm.js
+++ src/pages/clientPage/tasksPage/subtaskComponents/ImageForm.js
@@ -1,45 +1,56 @@
-import React from 'react'
-import { axiosOutHeaders } from '../../../..';
+import React from "react";
+import { axiosOutHeaders } from "../../../..";
+import { useParams } from "react-router-dom";
 
 export default function ImageForm({ onClose }) {
+  let { subtaskId } = useParams();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault(); // Prevent the default form submission behavior
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-        // Get the form data
-        const formData = new FormData(event.target);
+    // Get the form data
+    const formData = new FormData(event.target);
 
-        try {
-            // Make an asynchronous request to the server using fetch or axios
-            const response = await fetch('http://localhost:7777/upload/to-subtask/1', {
-                method: 'POST',
-                body: formData,
-            }, axiosOutHeaders);
-
-            // Handle the response as needed
-            console.log(response);
-        } catch (error) {
-            // Handle errors
-            console.error('Error:', error);
+    try {
+      // Make an asynchronous request to the server using fetch or axios
+      const response = await fetch(
+        `http://localhost:7777/upload/to-subtask/${subtaskId}`,
+        {
+          method: "POST",
+          body: formData,
+          ...axiosOutHeaders,
         }
-    };
-    return (
-        <div className='subtask-form'>
-            <form
-                action="http://localhost:7777/upload/to-subtask/1"
-                method="POST"
-                onSubmit={handleSubmit}
-                encType="multipart/form-data"
-                className='form-content'
-            >
-                <input type="file" name="image" id='image-box' accept="image/*" multiple />
-                <input type="submit" value="Upload"  />
-                <div className="subtask-form-buttons">
-                    <button className="custom-button" onClick={onClose}>
-                        <span></span>Close
-                    </button>
-                </div>
-            </form>
+      );
+
+      // Handle the response as needed
+      console.log(response);
+    } catch (error) {
+      // Handle errors
+      console.error("Error:", error);
+    }
+  };
+  return (
+    <div className="subtask-form">
+      <form
+        method="POST"
+        onSubmit={handleSubmit}
+        encType="multipart/form-data"
+        className="form-content"
+      >
+        <input
+          type="file"
+          name="image"
+          id="image-box"
+          accept="image/*"
+          multiple
+        />
+        <input type="submit" value="Upload" />
+        <div className="subtask-form-buttons">
+          <button className="custom-button" onClick={onClose}>
+            <span></span>Close
+          </button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
